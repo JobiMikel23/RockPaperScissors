aeeaf976afb49389a0d828e7ee6186fac8c11e8a
Mikel <115723692+JobiMikel23@users.noreply.github.com>
2022-10-25 20:42:26 +0100

    Create playerSelection with user input

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let playerSelection = (prompt("Please enter your weapon of choice, rock, paper or scissors"));
+playerSelection = playerSelection.toUpperCase();
+console.log("player choice is " + playerSelection);
+
 function getComputerChoice(){
     choice = Math.floor(Math.random() * 3)
     if (choice == 0){
