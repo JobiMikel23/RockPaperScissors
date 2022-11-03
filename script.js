aa39b277425ac9591e2a00cfb0a3ca3b9b9caf83
Mikel <115723692+JobiMikel23@users.noreply.github.com>
2022-11-03 11:55:30 +0000

    Clean up code

--- script.js
+++ script.js
@@ -1,45 +1,4 @@
 
-// const buttons = document.querySelectorAll('button');
-
-// testing button ids 
-// function clickPlayerChoice(){
-//     if (rockBtn.id === "Rock"){
-//         console.log("Rock");
-//     }
-// }
-
-//Iterate through the buttons, using for each. Add event listener to each button to add getPlayerChoice function
-// buttons.forEach((button) => {
-    
-
-
-//     button.addEventListener('click', function(){
-//       const playerSelection = getPlayerChoice();
-//       const computerSelection = "ROCK";//getComputerChoice();
-
-//       playRound(playerSelection,computerSelection);
-      
-//     });
-
-    
-
-// });
-
-
-
-
-//     // let choice = (prompt("Please enter your weapon of choice, rock, paper or scissors"));
-//     // choice = choice.toUpperCase();
-//     // // console.log(choice);
-//     // return choice;
-// }
-
-
-//getPlayerChoice function, asks the user for an input. Assigns this input
-// to choice variable, makes the choice uppercase so no matter the users input 
-// The choices will be the same 
-// return this choice. 
-
 function getComputerChoice(){
     choice = Math.floor(Math.random() * 3)
     if (choice == 0){
@@ -56,15 +15,32 @@ function getComputerChoice(){
 
 }
 
-// getComputerChoice function, generates a random number between 0-2 inclusive
-// assign to variable choice, if choice is equal to a certain number assign computers turn
+function getPlayerChoice(){
+    let choice = "";
+    if (buttonClick == "Rock"){
+        choice = "ROCK";
+        console.log(choice);
+        return choice;
+    }
+    else if (buttonClick == "Paper"){
+        choice = "PAPER";
+        console.log(choice);
+        return choice;
+    }
+    else if (buttonClick = "Scissors"){
+        choice = "SCISSORS";
+        console.log(choice);
+        return choice;
+    }
+    
+    
+}
+
 const container = document.querySelector('#results');
 const results = document.createElement('h3');
-
-
-
 results.style.color = "red";
 results.style.fontFamily = "Roboto";
+//For below function, red text to appear 
 
 function playRound(playerSelection, computerSelection){
 
@@ -105,16 +81,6 @@ function playRound(playerSelection, computerSelection){
 }
 
 
-
-//Select the container 
-
-//create element
-
-
-//playRound function, with the playerSelection and computerSelection as parameters
-//The playerChoice and computerChoice will be used as arguments for this 
-// use if statements to cover the draw, lose and win scenario. Win scenario only needs 
-// else as everything else has been covered.
 let playerScore = 0;
 let computerScore = 0;
 let draws = 0;
@@ -123,13 +89,13 @@ const displayScores = document.createElement('h4');
 displayScores.style.color = "blue";
 displayScores.style.fontFamily = "Roboto";
 const scores = document.querySelector('#scores');
-
+//Display score
 const winner = document.querySelector('#winner')
 const displayWinner = document.createElement('h2');
 displayWinner.style.color = "purple";
 displayWinner.style.fontFamily = "Roboto";
 
-
+//Display winner in below function
 
 function game2(currentResult){
     
@@ -163,75 +129,7 @@ function game2(currentResult){
 }
 
 
-
-
-
-function game(){
-
-    let wins = 0;
-    let draws = 0;
-    let losses = 0;
-    
-        
-
-    for (let i = 0; i < 5; i++){
-
-        
-        const playerSelection = getPlayerChoice();
-        
-        const computerSelection = getComputerChoice();
-
-        const currentResult = playRound(playerSelection,computerSelection);
-        
-        if(currentResult == "draw"){
-            draws += 1;
-            console.log("THIS ROUND IS A DRAW YOU BOTH PICKED " + playerSelection);
-        }
-        else if(currentResult == "lose"){
-            losses +=1;
-            console.log("YOU LOSE THIS ROUND HA HA ! YOU PICKED " + playerSelection + " AND THE COMPUTER PICKED " + computerSelection);
-        }
-        else{
-            wins+=1;
-            console.log("YOU WIN THIS ROUND! YOU PICKED " + playerSelection + " AND THE COMPUTER PICKED " + computerSelection);
-        } 
-            
-        
-        // console.log("wins is " +  wins + " losses is " + losses + " draws is " + draws);
-    }
-    if (wins > losses){
-        console.log("CONGRATULATIONS, YOU HAVE WON THE SET OF ROCK PAPER SCISSORS\nYOU GOT " + wins + " POINTS AND THE COMPUTER GOT "
-        + losses + " POINTS");
-    }
-    else if (losses > wins){
-        console.log("UNLUCKY LOSER, YOU LOST THE SET OF ROCK PAPER SCISSORS TO THE COMPUTER HA HA !\nYOU GOT " + losses + 
-        "AND THE COMPUTER GOT "+ wins + " POINTS" );
-    }else{
-        console.log("BORING! IT'S A DRAW \n YOU BOTH GOT " + draws + " POINTS");
-    }
-
-}
-
-
-// game();
-
-
-// console.log(playRound(playerSelection,computerSelection));
-
-//call the getPlayerChoice function and assign the return variable to playerSelection
-//to be used as an argument
-//do the same with the getComputerChoicefunction
-//Print the result of running playRound function with the relevant arguments
-
-
-// getPlayerChoice();
-// getComputerChoice();
-
-// const buttons = document.querySelectorAll('button');
-
-// buttons.forEach((button) => {
-//     button.addEventListener('click', getPlayerChoice);
-// });
+//Event listeners for buttons, calling a function on the event listeners. 
 
 let buttonClick = "";
 
@@ -264,23 +162,3 @@ scissorBtn.addEventListener('click', function(){
     game2(currentResult);
 });
 
-function getPlayerChoice(){
-    let choice = "";
-    if (buttonClick == "Rock"){
-        choice = "ROCK";
-        console.log(choice);
-        return choice;
-    }
-    else if (buttonClick == "Paper"){
-        choice = "PAPER";
-        console.log(choice);
-        return choice;
-    }
-    else if (buttonClick = "Scissors"){
-        choice = "SCISSORS";
-        console.log(choice);
-        return choice;
-    }
-    
-    
-}
